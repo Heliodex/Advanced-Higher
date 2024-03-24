79e1819785e80f721a0a91281e9d936969b087bb
Lewin Kelly <heli@odex.cf>
2024-03-24 06:38:17 +0000

    Improvements to cookie parsing and formatting improvements

--- db.ts
+++ db.ts
@@ -10,14 +10,11 @@ const headers = { "content-type": "application/json" }
 // This basic proxy only exists because the main application, written in Luau, doesn't have the capability to interact with MySQL.
 
 Deno.serve({ port: 3307 }, async req => {
-	if (req.method !== "POST")
-		return new Response("Method not allowed", { status: 405 })
-
 	try {
 		const body = await req.json()
 		const result = await client.execute(body.query, body?.params)
 
-		return new Response(JSON.stringify(result, null, 2), { headers })
+		return new Response(JSON.stringify(result), { headers })
 	} catch (e) {
 		return new Response(JSON.stringify({ error: e.message }), {
 			status: 400,
